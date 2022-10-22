918e1ee6a705cf984d4fbf4ca7331d1ccbb3b889
Gabrielly Vieira dos Santos <50851101+gabiest@users.noreply.github.com>
2022-10-21 22:19:58 -0300

    Criação de Filtro e LocalStorage

--- appListaDeTarefas/listaDeTarefas.js
+++ appListaDeTarefas/listaDeTarefas.js
@@ -8,6 +8,8 @@ let taref = document.querySelector('#tarefa');
 
             btnadd.addEventListener('click',adicionaTarefa);
             lista.addEventListener('click', apagarTarefa);
+            Btnlimp.addEventListener('click', removetudo);
+            filtro.addEventListener('keyup', filtrarTarefa);
             
         }
 
@@ -31,7 +33,7 @@ let taref = document.querySelector('#tarefa');
 
               alert('vazioooo gatx');
              }
-            
+            else{
          const li = document.createElement('li');
          li.className = 'collection-item';  
          li.appendChild(document.createTextNode(taref.value));
@@ -46,16 +48,61 @@ let taref = document.querySelector('#tarefa');
          li.appendChild(a);
          lista.appendChild(li);
 
+         gravarTarefa(taref.value);
+
+         
+ 
+
          taref.value = '';
 
+        }
 
 
-
-
-
-            
 
         }
+
+        function removetudo(evento){
+
+            evento.preventDefault();
+            lista.remove();
+
+        }
+
+
+        function filtrarTarefa(evento) {
+            const procurado = evento.target.value.toLowerCase();
+            const liItem = document.querySelectorAll(".collection-item");
+        
+            liItem.forEach(function (tarefa) {
+                let textoTarefa = tarefa.innerText;
+        
+                if (textoTarefa.toLowerCase().indexOf(procurado) != -1) {
+                    tarefa.style.display = "block";
+                } else {
+                    tarefa.style.display = "none";
+                }
+            });
+
+        }
+
+        function gravarTarefa(tarefa){
+            
+         let tarefas = [];
+         let trfs = localStorage.getItem('tarefas');
+
+        if( trfs != null){
+
+            tarefas = JSON.parse(trfs);
+        };
+
+        tarefas.push(tarefa);
+        localStorage.setItem('tarefas',JSON.stringify(tarefas));
+        // localStorage.getItem('tarefas',JSON.parse(tarefas));
+
+        
+
+        }
+
 
 
 
